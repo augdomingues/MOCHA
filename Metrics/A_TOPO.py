b393896535435c3652074f0cb1bd999066b68679
Augusto Domingues <augsto.c.s@gmail.com>
2018-09-12 14:48:12 -0300

    Fixed TOPO extraction

--- Metrics/A_TOPO.py
+++ Metrics/A_TOPO.py
@@ -3,7 +3,6 @@
     every node in the trace.
 """
 from Metrics.Metric import Metric
-from Graph import Graph
 from mocha_utils import Encounter
 
 class A_TOPO(Metric):
@@ -12,12 +11,10 @@ class A_TOPO(Metric):
     def __init__(self, infile, outfile, report_id, **kwargs):
         self.topo = {}
         self.a_topo = {}
-        self.graph = Graph()
-        self.total_neighbors = {}
+        self.topologies = {}
         self.infile = infile
         self.outfile = outfile
         self.report_id = report_id
-
 
     def print(self):
         with open(self.outfile, "w+") as out:
@@ -33,43 +30,28 @@ class A_TOPO(Metric):
                 comps = line.strip().split(" ")
                 user1, user2 = comps[0], comps[1]
 
-                self.graph.add_vertex(user1)
-                self.graph.add_vertex(user2)
+                if user1 not in self.topologies:
+                    self.topologies[user1] = set()
+                self.topologies[user1].add(user2)
 
-                if not self.graph.contains_edge(user1, user2):
-                    self.graph.add_edge(user1, user2)
+                if user2 not in self.topologies:
+                    self.topologies[user2] = set()
+                self.topologies[user2].add(user1)
 
-        edges = self.graph.edge_set()
-        for edge in edges:
-            src = edge.src
-            trg = edge.target
-            enc = str(Encounter(int(src), int(trg)))
+        for source, source_neighbors in self.topologies.items():
+            for target, target_neighbors in self.topologies.items():
+                if source != target:
+                    encounter = str(Encounter(int(source), int(target)))
 
+                    intersec = source_neighbors.intersection(target_neighbors)
+                    intersec = len(intersec)
 
-            if enc not in self.total_neighbors:
-                self.total_neighbors[enc] = []
+                    union = source_neighbors.union(target_neighbors)
+                    union = len(union)
 
-            neighbors_src = self.graph.get_vertex(src).get_connections()
-            degree_src = len(neighbors_src)
+                    direct_connection = 1 if target in source_neighbors else 0
 
-            neighbors_trgt = self.graph.get_vertex(src).get_connections()
-            degree_dest = len(neighbors_trgt)
-
-            exists = 0
-            if self.graph.contains_edge(src, trg):
-                exists = 1
-
-            to = 0
-            for t in neighbors_trgt:
-                if t in neighbors_src:
-                    to += 1
-            numerator = float(to) + 1
-            denominator = ((degree_src - exists) + (degree_dest - exists) -to) +1
-            if denominator == 0:
-                denominator = 1
-
-            toPct = numerator/denominator
-            self.topo[enc] = toPct
+                    self.topo[encounter] = (intersec + direct_connection)/union
 
         for key, item in self.topo.items():
             nodea, nodeb = key.split(" ")
@@ -84,7 +66,6 @@ class A_TOPO(Metric):
 
         for key, item in self.a_topo.items():
             self.a_topo[key] = sum(item)/max(len(item), 1)
-
 
     def commit(self):
         return {}
--- Metrics/TOPO.py
+++ Metrics/TOPO.py
@@ -3,7 +3,7 @@
     the pair of contacts in the trace.
 """
 from Metrics.Metric import Metric
-from Graph import Graph
+# from Graph import Graph
 from mocha_utils import Encounter
 
 class TOPO(Metric):
@@ -11,8 +11,9 @@ class TOPO(Metric):
 
     def __init__(self, infile, outfile, report_id, **kwargs):
         self.topo = {}
-        self.graph = Graph()
-        self.total_neighbors = {}
+        # self.graph = Graph()
+        self.topologies = {}
+        # self.total_neighbors = {}
         self.infile = infile
         self.outfile = outfile
         self.report_id = report_id
@@ -33,43 +34,66 @@ class TOPO(Metric):
                 comps = line.strip().split(" ")
                 user1, user2 = comps[0], comps[1]
 
-                self.graph.add_vertex(user1)
-                self.graph.add_vertex(user2)
+                # self.graph.add_vertex(user1)
+                # self.graph.add_vertex(user2)
 
-                if not self.graph.contains_edge(user1, user2):
-                    self.graph.add_edge(user1, user2)
+                ###
+                if user1 not in self.topologies:
+                    self.topologies[user1] = set()
+                self.topologies[user1].add(user2)
 
-        edges = self.graph.edge_set()
-        for edge in edges:
-            src = edge.src
-            trg = edge.target
-            enc = str(Encounter(int(src), int(trg)))
+                if user2 not in self.topologies:
+                    self.topologies[user2] = set()
+                self.topologies[user2].add(user1)
+                ###
 
+                # if not self.graph.contains_edge(user1, user2):
+                #     self.graph.add_edge(user1, user2)
 
-            if enc not in self.total_neighbors:
-                self.total_neighbors[enc] = []
+        ###
+        for source, source_neighbors in self.topologies.items():
+            for target, target_neighbors in self.topologies.items():
+                if source != target:
+                    encounter = str(Encounter(int(source), int(target)))
 
-            neighbors_src = self.graph.get_vertex(src).get_connections()
-            degree_src = len(neighbors_src)
+                    intersec = source_neighbors.intersection(target_neighbors)
+                    intersec = len(intersec)
 
-            neighbors_trg = self.graph.get_vertex(src).get_connections()
-            degree_dest = len(neighbors_trg)
+                    union = source_neighbors.union(target_neighbors)
+                    union = len(union)
 
-            exists = 0
-            if self.graph.contains_edge(src, trg):
-                exists = 1
+                    direct_connection = 1 if target in source_neighbors else 0
 
-            to = 0
-            for target in neighbors_trg:
-                if target in neighbors_src:
-                    to += 1
-            numerator = float(to) + 1
-            denominator = ((degree_src - exists) + (degree_dest - exists) -to) +1
-            if denominator == 0:
-                denominator = 1
+                    self.topo[encounter] = (intersec + direct_connection)/union
+        ###
+        # edges = self.graph.edge_set()
+        # for edge in edges:
+            # src = edge.src
+            # trg = edge.target
+            # enc = str(Encounter(int(src), int(trg)))
 
-            percent = numerator/denominator
-            self.topo[enc] = percent
+            # if enc not in self.total_neighbors: # Why is this here?
+            #     self.total_neighbors[enc] = []
+
+            # neighbors_src = self.graph.get_vertex(src).get_connections()
+            # degree_src = len(neighbors_src)
+
+            # neighbors_trg = self.graph.get_vertex(src).get_connections()
+            # degree_dest = len(neighbors_trg)
+
+            # exists = 0
+            # if self.graph.contains_edge(src, trg):
+            #     exists = 1
+
+            # to = 0
+            # for target in neighbors_trg:
+            #     if target in neighbors_src:
+            #         to += 1
+            # numerator = float(to) + 1
+            # denominator = ((degree_src - exists) + (degree_dest - exists) -to) +1
+
+            # percent = numerator/max(1, denominator)
+            # self.topo[enc] = percent
 
     def commit(self):
         values = {"TOPO": self.topo}
